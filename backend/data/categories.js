7060badf0c9836c162feb4712ef8e8feb774d9a1
kucse160235 <afnan1635@cseku.ac.bd>
2022-05-31 19:02:17 +0600

    Update Seeder.js and Data Folder

---
+++ backend/data/categories.js
@@ -0,0 +1,28 @@
+const categories = [
+  {
+    _id: '6296001fa3fc2b38ee5f23d6',
+    name: 'Man',
+    slug: 'Man',
+  },
+
+  {
+    _id: '62960055a3fc2b38ee5f23dc',
+    name: 'Shirts',
+    slug: 'Shirts',
+    parentId: '6296001fa3fc2b38ee5f23d6',
+  },
+  {
+    _id: '6296074a50f5a7789c69dbfe',
+    name: 'Electronics>>Wearable',
+    slug: 'Electronics',
+  },
+
+  {
+    _id: '6296076a50f5a7789c69dc01',
+    name: 'Accessories',
+    slug: 'Accessories',
+    parentId: '6296074a50f5a7789c69dbfe',
+  },
+];
+
+export default categories;
--- backend/seeder.js
+++ backend/seeder.js
@@ -3,10 +3,9 @@ import mongoose from 'mongoose';
 import colors from 'colors';
 
 import users from './data/users.js';
-import products from './data/products.js';
+import categories from './data/categories.js';
 import User from './models/userModel.js';
-import Order from './models/orderModel.js';
-import Product from './models/productModel.js';
+import Category from './models/categoryModel.js';
 
 import connectDB from './config/db.js';
 
@@ -16,19 +15,12 @@ connectDB();
 
 const importData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
+    await Category.deleteMany();
     await User.deleteMany();
 
-    const createdUsers = await User.insertMany(users);
+    await User.insertMany(users);
 
-    const adminUser = createdUsers[0]._id;
-
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
-
-    await Product.insertMany(sampleProducts);
+    await Category.insertMany(categories);
 
     console.log('Data Imported'.green.inverse);
     process.exit();
@@ -40,8 +32,7 @@ const importData = async () => {
 
 const destroyData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
+    await Category.deleteMany();
     await User.deleteMany();
 
     console.log('Data Destroyed!'.red.inverse);
